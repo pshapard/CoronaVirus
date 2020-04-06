7ff68d53f604e0ab80fec6100f87e9a2f6e7308a
Patrick Shapard <pshapard@users.noreply.github.com>
2020-04-05 23:20:23 -0500

    Added variable to CalcRate class
    added recover rate variable with default setting of None

--- ClassesFuncs.py
+++ ClassesFuncs.py
@@ -1,35 +1,42 @@
-#Created: 03/27/2020
-#Updated: 04/03/2020
-#Module for corona virus classes and functions
+# Created: 03/27/2020
+# Updated: 04/03/2020
+# Module for corona virus classes and functions
 
 import sys
 import logging
 import time
 
-__version__ = '1.03'
+__version__ = '1.07'
 __author__ = 'Patrick R. Shapard'
 
-class CalcMortalRate(object):
-    """Class object to calculate corona virus death rate in %. """
-    def __init__(self, num_cases, num_deaths):
+
+class CalcRate(object):
+    """Class object to calculate corona virus death and recovery rate in %. """
+
+    def __init__(self, num_cases, num_deaths, num_recover=None):
         self.num_cases = num_cases
         self.num_deaths = num_deaths
-    
-    def calc(self):
-        """Function calculates the corona virus death rate. """
+        self.num_recover = num_recover
+
+    def death(self):
+        """Function returns virus death rate. """
         try:
-            return ((self.num_deaths/self.num_cases)*100)
+            return ((self.num_deaths / self.num_cases) * 100)
         except ZeroDivisionError:
             logging.error("Function attempted to devide by zero")
             sys.exit(0)
 
+    def recover(self):
+        """Function returns virus recovery rate """
+        return((self.num_recover / self.num_cases) * 100)
+
 
 def create_log_file(filename, level=logging.DEBUG):
     """Function creates log files and is used in setup_logging_Enhanced(file). """
-    TimeStamp = time.strftime("%Y%m%d_%H%M%S")
     handler = logging.FileHandler(filename)
     handler.setLevel(level)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
     handler.setFormatter(formatter)
     logging.getLogger('').addHandler(handler)
 
@@ -50,17 +57,22 @@ def setup_logging_Enhanced(file):
     console.setFormatter(formatter)
     # add the handler to the root logger
     logging.getLogger('').addHandler(console)
-    #open up a few files for logging at different levels
-    create_log_file('logs/' + TimeStamp + components + '_DEBUG.log', logging.DEBUG)
-    create_log_file('logs/' + TimeStamp + components + '_INFO.log', logging.INFO)
-    create_log_file('logs/' + TimeStamp + components + '_WARNING.log', logging.WARNING)
-    create_log_file('logs/' + TimeStamp + components + '_ERROR.log', logging.ERROR)
+    # open up a few files for logging at different levels
+    create_log_file('logs/' + TimeStamp + components +
+                    '_DEBUG.log', logging.DEBUG)
+    create_log_file('logs/' + TimeStamp + components +
+                    '_INFO.log', logging.INFO)
+    create_log_file('logs/' + TimeStamp + components +
+                    '_WARNING.log', logging.WARNING)
+    create_log_file('logs/' + TimeStamp + components +
+                    '_ERROR.log', logging.ERROR)
     #create_log_file('logs/' + TimeStamp + components + '_CRITICAL.log', logging.CRITICAL)
     #create_log_file('logs/' + TimeStamp + components + '_TESTCASES.log', logging.TESTCASES)
-    #logging.basicConfig() 
+    # logging.basicConfig()
     requests_log = logging.getLogger("requests.packages.urllib3")
     requests_log.setLevel(logging.DEBUG)
     requests_log.propagate = True
+
 
 def countdown(t):
     """Timer function, receives 1 variable in seconds """
@@ -70,4 +82,4 @@ def countdown(t):
         print(timeformat, end='\r')
         time.sleep(1)
         t -= 1
-    print('Goodbye!\n\n\n\n\n')
+    print('Continue!\n\n')
